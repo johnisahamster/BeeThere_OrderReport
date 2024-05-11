1c495670d1b74a868cf51b1e4abc3a0d1876da1f
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-05-11 08:06:47 -0400

    Add project files.

---
+++ BeeThere_OrderReport/BeeThere_OrderReport/Classes/Square/Sandbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeThere_OrderReport.Classes.Square
+{
+    class Sandbox : SquareEnvironment
+    {
+        public Sandbox() {
+            this.application_id = "sandbox-sq0idb-_6v-fo4O7Zlc6jZqJaHw9Q";
+            this.access_token = "EAAAl-nSZ6Mn0-yb8x08HvZ4_GdYoyq8aQoHlRv7OtUmJbYYCwmQjHpl1AMKTKCW";
+        }
+
+    }
+}
---
+++ BeeThere_OrderReport/BeeThere_OrderReport/Classes/Square/SquareEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeThere_OrderReport.Classes.Square
+{
+    class SquareEnvironment
+    {
+        public SquareEnvironment() { }
+
+        protected string application_id;
+        protected string access_token;
+
+        public string getAppID() { return application_id; }
+        public string getAccessToken() { return access_token; }
+    }
+}
